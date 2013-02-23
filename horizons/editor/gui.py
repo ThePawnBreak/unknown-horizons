eea794ed871b9a51abdc676f737a9a70ca5e5daf
ThePawnBreak <mocanu.cristian93@gmail.com>
2013-02-23 22:46:17 +0200

    Replace dummy of message_widget with real instance.
    This is done because message_widget was needed to show messages in the editor. A logbook dummy is created in order for message_widget to work.

--- horizons/editor/gui.py
+++ horizons/editor/gui.py
@@ -31,6 +31,7 @@ from horizons.gui.tabs import TabWidget
 from horizons.gui.tabs.tabinterface import TabInterface
 from horizons.gui.util import load_uh_widget
 from horizons.gui.widgets.imagebutton import OkButton, CancelButton
+from horizons.gui.widgets.messagewidget import MessageWidget
 from horizons.gui.widgets.minimap import Minimap
 from horizons.gui.windows import WindowManager, Window
 from horizons.util.lastactiveplayersettlementmanager import LastActivePlayerSettlementManager
@@ -65,6 +66,8 @@ class IngameGui(LivingObject):
 		# Mocks needed to act like the real IngameGui
 		self.show_menu = Dummy
 		self.hide_menu = Dummy
+		# a logbook Dummy is necessary for message_widget to work
+		self.logbook = Dummy
 
 		self.mainhud = load_uh_widget('minimap.xml')
 		self.mainhud.position_technique = "right+0:top+0"
@@ -92,6 +95,7 @@ class IngameGui(LivingObject):
 			self.mainhud.findChild(name=widget).hide()
 
 		self.windows = WindowManager()
+		self.message_widget = MessageWidget(self.session)
 		self.save_map_dialog = SaveMapDialog(self.session, self.windows)
 		self.pausemenu = PauseMenu(self.session, self, self.windows, in_editor_mode=True)
 		self.help_dialog = HelpDialog(self.windows, session=self.session)
